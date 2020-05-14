6e72e4e5f5c6d2402c092c54a479ed4478e9c6ee
Treat Metcalf <treatmet@gmail.com>
2020-05-14 17:02:40 -0500

    more removals

--- start-nodejs-PC.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-D:
-cd D:\GoogleDrive\Projects\racewar3000
-node app.js 3000 true true
-echo ---------------------------------------------
-echo !!! SERVER CRASH! Press ENTER to continue !!!
-echo !!! SERVER CRASH! Press ENTER to continue !!!
-set /p id="!!! SERVER CRASH! Press ENTER to continue !!!"
