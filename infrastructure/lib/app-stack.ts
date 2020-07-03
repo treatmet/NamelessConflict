e0e1ae2675c56a5ea405d6977baa6d717546ba16
Preston Robertson <prestonwilliamrobertson@gmail.com>
2020-07-02 20:48:29 -0500

    Add app security group and load balancer

--- infrastructure/lib/app-stack.ts
+++ infrastructure/lib/app-stack.ts
@@ -1,6 +1,6 @@
 import { Stack, StackProps, Construct } from '@aws-cdk/core';
-import { ApplicationLoadBalancer } from '@aws-cdk/aws-elasticloadbalancingv2';
-import { Vpc } from '@aws-cdk/aws-ec2';
+import { ApplicationLoadBalancer, IpAddressType } from '@aws-cdk/aws-elasticloadbalancingv2';
+import { Vpc, SecurityGroup, Port, Protocol, Peer } from '@aws-cdk/aws-ec2';
 
 export class AppStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
@@ -8,6 +8,31 @@ export class AppStack extends Stack {
 
     const defaultVpc = Vpc.fromLookup(this, "defaultVpc", {
       isDefault: true
+    });
+
+    const securityGroup = new SecurityGroup(this, "SG-DefaultExternal", {
+      securityGroupName: "SG-DefaultExternal",
+      description: "Default security group for external-facing web services",
+      vpc: defaultVpc,
+      allowAllOutbound: true,
+    });
+
+    const httpsPort = new Port({
+      protocol: Protocol.TCP,
+      fromPort: 443,
+      toPort: 443,
+      stringRepresentation: "HTTPS"
+    })
+
+    securityGroup.addIngressRule(Peer.anyIpv4(), httpsPort);
+    securityGroup.addIngressRule(Peer.anyIpv6(), httpsPort);
+
+    const gameServiceLB = new ApplicationLoadBalancer(this, "LB-GameService", {
+      loadBalancerName: "LB-GameService",
+      vpc: defaultVpc,
+      internetFacing: true,
+      ipAddressType: IpAddressType.DUAL_STACK,
+      securityGroup: securityGroup
     });
   }  
 }
