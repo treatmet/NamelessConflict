187f4a0330002f9e1fafbdedaad93827e01c68cf
Preston Robertson <prestonwilliamrobertson@gmail.com>
2020-07-19 08:56:40 -0500

    Add web client; fix some issues

--- example/sticky/server.js
+++ example/sticky/server.js
@@ -1,16 +1,39 @@
 'use strict';
 
 const express = require('express');
+var cookieParser = require('cookie-parser');
 
 // Constants
-const PORT = 8080;
+const PORT = 80;
 const HOST = '0.0.0.0';
-
+const instance = Math.floor(Math.random() * 1000);
 // App
 const app = express();
-app.get('/', (req, res) => {
-  res.send('Hello World');
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.static('client'))
+
+app.get('/instance', (req, res) => {
+  res.send({instance});
+});
+
+app.get('/clear', (req, res) => {
+  res.clearCookie('AWSALB');
+  res.clearCookie('AWSALBCORS');
+
+  res.send({instance});
+});
+
+app.get('/set/:cookie', (req, res) => {
+  res.cookie('AWSALB', req.params.cookie);
+  res.cookie('AWSALBCORS', req.params.cookie);
+
+  res.send({
+    instance,
+    previous: req.cookies.AWSALB,
+    current: req.params.cookie
+  });
 });
 
 app.listen(PORT, HOST);
-console.log(`Running on http://${HOST}:${PORT}`);
+console.log(`Running on http://${HOST}:${PORT} as instance ${instance}`);
--- infrastructure/lib/poc-stack.ts
+++ infrastructure/lib/poc-stack.ts
@@ -2,6 +2,7 @@ import { Stack, StackProps, Construct } from '@aws-cdk/core';
 import { ContainerImage } from '@aws-cdk/aws-ecs';
 import { ApplicationLoadBalancedFargateService } from '@aws-cdk/aws-ecs-patterns';
 import { Role } from '@aws-cdk/aws-iam';
+import { Vpc } from '@aws-cdk/aws-ec2';
 
 export class PocStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
@@ -9,19 +10,19 @@ export class PocStack extends Stack {
 
     const image = ContainerImage.fromAsset("../example/sticky");
 
-    const taskExecutionRole = Role.fromRoleArn(this, "taskRole", 
-      "arn:aws:iam::231793983438:role/aws-service-role/ecs.amazonaws.com/AWSServiceRoleForECS");
+    const defaultVpc = Vpc.fromLookup(this, "defaultVpc", {
+      isDefault: true
+    });
 
     const fargateService = new ApplicationLoadBalancedFargateService(this, "PocService", {
       assignPublicIp: true,
       desiredCount: 2,
       cpu: 256,
       memoryLimitMiB: 512,
+      vpc: defaultVpc,
       taskImageOptions: {
         image: image,
-        // TODO: change this role to include whatever permissions the app needs
-        taskRole: taskExecutionRole,
-        executionRole: taskExecutionRole
+        enableLogging: false
       },
     });
   }  
