e092b18b320013f21e466fbd806e27eebae36f71
Treat Paine Metcalf <treatmet@gmail.com>
2020-07-31 00:31:13 -0500

    Verified Lambda upating EB!

--- lambda/index.js
+++ lambda/index.js
@@ -1,8 +1,13 @@
 global.mongoDbLocation = process.env.mongoDbLocation;
 global.s3LoggingBucket = process.env.s3LoggingBucket;
 
+var AWS = require("aws-sdk");
+var elasticbeanstalk = new AWS.ElasticBeanstalk();
+
 require('./code/logEngine.js');
 const dataAccess = require('./code/dataAccess.js');
+
+
 
 exports.handler = (event, context, callback) => {
     context.callbackWaitsForEmptyEventLoop = false;
@@ -28,14 +33,73 @@ exports.handler = (event, context, callback) => {
         console.log("fullServers: " + fullServers);
         console.log("emptyServers: " + emptyServers);
         console.log("playableServers: " + playableServers);
+        updateInstanceCount(3, function(){
+            console.log("updateInstnceCount callback");
+            callback(null, "fullServers: " + fullServers);
+        });
 
-        callback(null, "fullServers: " + fullServers);
 
     });	
     
 
 
 };
+
+function updateInstanceCount(targetCount, cb){
+    var params = {
+        EnvironmentName: "BeanstalkNodePoc", 
+        OptionSettings: [
+            {
+                Namespace: "aws:autoscaling:asg", 
+                OptionName: "MinSize", 
+                Value: targetCount.toString()
+            }, 
+            {
+                Namespace: "aws:autoscaling:asg", 
+                OptionName: "MaxSize", 
+                Value: targetCount.toString()
+            } 
+        ]
+    };
+
+    elasticbeanstalk.updateEnvironment(params, function(err, data) {
+        if (err){ 
+            console.log("ERROR FAILED BEANSTALK UPDATE");
+            console.log(err, err.stack); // an error occurred
+        }
+        else {
+            console.log("BEANSTALK UPDATE SUCCESS!!!");
+            console.log(data);           // successful response
+        }
+        cb();
+
+        /*
+        "OptionName": "MinSize",
+        "OptionName": "MaxSize"
+
+        data = {
+         ApplicationName: "my-app", 
+         CNAME: "my-env.elasticbeanstalk.com", 
+         DateCreated: <Date Representation>, 
+         DateUpdated: <Date Representation>, 
+         EndpointURL: "awseb-e-i-AWSEBLoa-1RDLX6TC9VUAO-0123456789.us-west-2.elb.amazonaws.com", 
+         EnvironmentId: "e-szqipays4h", 
+         EnvironmentName: "my-env", 
+         Health: "Grey", 
+         SolutionStackName: "64bit Amazon Linux running Tomcat 7", 
+         Status: "Updating", 
+         Tier: {
+          Name: "WebServer", 
+          Type: "Standard", 
+          Version: " "
+         }, 
+         VersionLabel: "v2"
+        }
+        */
+        
+      });
+
+}
 
 function getPublicServersFromDB(cb){
 	var servers = [];
