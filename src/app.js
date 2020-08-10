530956f1e60bf0028b145e270edd2d0f459b5718
Preston Robertson <prestonwilliamrobertson@gmail.com>
2020-08-10 06:37:54 -0500

    Move shared components into src folder

---
+++ src/app.js
@@ -0,0 +1,26 @@
+const { spawn } = require('child_process');
+
+const processes = [
+  {
+    name: "web1",
+    process: spawn('node', ['./packages/hello-service/app.js', '8080'])
+  },
+  {
+    name: "game1",
+    process: spawn('node', ['./packages/hello-service/app.js', '3001'])
+  }
+];
+
+processes.map(p => {
+  p.process.stdout.on('data', (data) => {
+    console.log(`[${p.name}] ${data}`);
+  });
+  
+  p.process.stderr.on('data', (data) => {
+    console.error(`[${p.name}] ${data}`);
+  });
+  
+  p.process.on('close', (code) => {
+    console.log(`[${p.name}] child process exited with code ${code}`);
+  });
+});
